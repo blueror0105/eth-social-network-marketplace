4013d1f15df6e9cf241204b6365c2b5af27e57d2
Blue <91778766+bllue0313@users.noreply.github.com>
2021-12-13 22:42:55 +0100

    First deploy of social network contract

---
+++ src/contracts/SocialNetwork.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract SocialNetwork {
+    // State variable
+    string public name;
+
+    constructor() public {
+        name = "Dapp University Social Network";
+    }
+}
