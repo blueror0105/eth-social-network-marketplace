1a194ba3f2eb34bcafe2c8ec67ba7d6beba33032
Blue <91778766+bllue0313@users.noreply.github.com>
2021-12-14 03:06:57 +0100

    Added unit test for social network contract

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -4,7 +4,23 @@ contract SocialNetwork {
     // State variable
     string public name;
 
+    struct Post {
+        uint256 id;
+        string content;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    uint256 public postCount = 0;
+
+    mapping(uint256 => Post) public posts;
+
     constructor() public {
         name = "Dapp University Social Network";
+    }
+
+    function createPost(string memory _content) public {
+        postCount++;
+        posts[postCount] = Post(postCount, _content, 0, msg.sender);
     }
 }
---
+++ test/SocialNetwork.test.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+
+const SocialNetwork = artifacts.require('./SocialNetwork.sol');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should();
+
+contract('SocialNetwork', (accounts) => {
+    let socialnetwork;
+
+    before(async () => {
+        socialnetwork = await SocialNetwork.deployed();
+    });
+
+    describe('deployment', async () => {
+        it('deploys successfully', async () => {
+            const address = await socialnetwork.address;
+            assert.notEqual(address, 0x0);
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        });
+
+        it('has a name', async () => {
+            const name = await socialnetwork.name();
+            assert.equal(name, 'Dapp University Social Network')
+        })
+    });
+})
